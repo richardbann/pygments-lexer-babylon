0872457cef0cdeb92ddfa8de0b87c734e2a55430
Richard Bann <richard.bann@vertis.com>
2016-02-25 17:15:36 +0100

    improve test runner

--- run_test.sh
+++ run_test.sh
@@ -14,8 +14,11 @@ ret=$(($ret + $?))
 docker-compose -f ${dir}/docker/test-compose.yml run --rm babylonlexer-node10 test
 ret=$(($ret + $?))
 
-if [($ret)] then
+if [ "$ret" -gt 0 ]; then
+    echo $(tput setaf 1)Tests failed...$(tput sgr0)
     exit 1
 fi;
+
+echo $(tput setaf 2)Success...$(tput sgr0)
 
 docker-compose -f ${dir}/docker/test-compose.yml run --rm babylonlexer-py3 covreport
